96056f569c58ac55bf0952aad127b600d80c6ef7
Jiří Wolker <projects@jwo.cz>
2023-03-22 10:53:34 +0000

    Disable flash customization when it is disabled

--- app/src/main/java/cz/jwo/kisctecka/SettingsActivity.kt
+++ app/src/main/java/cz/jwo/kisctecka/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package cz.jwo.kisctecka
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SeekBarPreference
+import androidx.preference.TwoStatePreference
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +20,34 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+        private lateinit var flashEnabledPref: TwoStatePreference
+        private lateinit var flashOnlyOnSuccess: TwoStatePreference
+        private lateinit var flashBrightnessPref: SeekBarPreference
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
-            findPreference<SeekBarPreference>("flash_brightness")?.let {
-                it.isVisible = MainActivity.getTorchBrightnessRegulationAvailable(requireContext())
+            flashBrightnessPref = findPreference<SeekBarPreference>("flash_brightness")!!
+            flashEnabledPref = findPreference<TwoStatePreference>("flash_on_read")!!
+            flashOnlyOnSuccess = findPreference<TwoStatePreference>("flash_only_on_success")!!
+
+            flashBrightnessPref.isVisible = MainActivity.getTorchBrightnessRegulationAvailable(requireContext())
+
+            refreshPreferenceDependencies()
+
+            preferenceManager.sharedPreferences?.registerOnSharedPreferenceChangeListener (this)
+        }
+
+        private fun refreshPreferenceDependencies() {
+            flashEnabledPref.isChecked.let {
+                flashBrightnessPref.isEnabled = it
+                flashOnlyOnSuccess.isEnabled = it
             }
+        }
+
+        override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+            refreshPreferenceDependencies()
         }
     }
 }
