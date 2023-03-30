cadede993a7042994e36517320aafd00dc14c88d
Jiří Wolker <projects@jwo.cz>
2023-03-30 11:32:27 +0000

    Show error screen when NFC is not available

--- app/src/main/java/cz/jwo/kisctecka/MainActivity.kt
+++ app/src/main/java/cz/jwo/kisctecka/MainActivity.kt
@@ -12,7 +12,6 @@ import android.hardware.camera2.CameraManager
 import android.nfc.NfcAdapter
 import android.nfc.tech.MifareClassic
 import android.nfc.tech.MifareUltralight
-import android.os.Build
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.os.Bundle
@@ -45,7 +44,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var logView: TextView
     private val readerStateBroadcastReceiver = ReaderStateBroadcastReceiver()
 
-    private lateinit var nfcAdapter: NfcAdapter
+    private var nfcAdapter: NfcAdapter?=null
 
     private lateinit var statusTextView: TextView
 
@@ -92,7 +91,15 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
         }
 
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        NfcAdapter.getDefaultAdapter(this).takeUnless { resources.getBoolean(R.bool.pretend_having_no_nfc) }.let {
+            if (it!=null) {
+                nfcAdapter  =it
+            } else {
+                finish()
+                startActivity(Intent(this@MainActivity, NfcNotAvailableActivity::class.java))
+                return
+            }
+        }
         cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
         powerManager = getSystemService(POWER_SERVICE) as PowerManager
 
@@ -128,7 +135,7 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
 
         Log.d(TAG, "Disabling foreground NFC dispatch.")
-        nfcAdapter.disableForegroundDispatch(this)
+        nfcAdapter?.disableForegroundDispatch(this)
 
         proximityWakeLock?.release()
     }
@@ -150,7 +157,7 @@ class MainActivity : AppCompatActivity() {
             if (VERSION.SDK_INT >= VERSION_CODES.S) PendingIntent.FLAG_MUTABLE else 0
         )
 
-        nfcAdapter.enableForegroundDispatch(
+        nfcAdapter?.enableForegroundDispatch(
             this,
             pendingIntent,
             null,
---
+++ app/src/main/java/cz/jwo/kisctecka/NfcNotAvailableActivity.kt
@@ -0,0 +1,11 @@
+package cz.jwo.kisctecka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NfcNotAvailableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nfc_not_available)
+    }
+}
